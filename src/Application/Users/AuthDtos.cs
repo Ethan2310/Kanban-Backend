b38b7ff13abb4f7a73c86b62bda160c274febd49
Ethan Michael Varaden <ethanvaraden@gmail.com>
2026-04-15 03:58:07 +0200

    implement auth endpoints for user login/register

---
+++ src/Application/Users/AuthDtos.cs
@@ -0,0 +1,30 @@
+using Domain.Enumerations;
+
+namespace Application.Users;
+
+public record RegisterRequest(
+    string Email,
+    string Password,
+    string FirstName,
+    string LastName,
+    UserRole Role,
+    int? AddedById);
+
+public record RegisterResponse(
+    int UserId,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Role);
+
+public record LoginRequest(
+    string Email,
+    string Password);
+
+public record AuthResponse(
+    string Token,
+    int UserId,
+    string Email,
+    string FirstName,
+    string LastName,
+    string Role);
---
+++ src/Application/Users/AuthValidator.cs
@@ -0,0 +1,40 @@
+using FluentValidation;
+
+namespace Application.Users;
+
+public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(255);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(100);
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty()
+            .MaximumLength(100);
+
+        RuleFor(x => x.LastName)
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
+
+public class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
